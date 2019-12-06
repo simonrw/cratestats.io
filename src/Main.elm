a887da363a393f868a71abdd2b84b46b34118cec
Simon Walker <s.r.walker101@googlemail.com>
2019-12-06 16:13:22 +0000

    Set up fetching crate details
    This does not work with parcel's default server, but I will try from
    netlify

--- src/Main.elm
+++ src/Main.elm
@@ -5,11 +5,17 @@ import Html exposing (Html, button, div, text, h1, input, Attribute, h2)
 import Html.Events exposing (onInput, on, keyCode)
 import Html.Attributes exposing (type_, placeholder)
 import Json.Decode as D
+import Http
+
+type alias CrateDetails =
+    {}
+
 
 
 type alias Model =
     { currentText : String
     , crate : String
+    , crateDetails : Maybe CrateDetails
     }
 
 main =
@@ -25,12 +31,14 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { currentText = ""
     , crate = ""
+    , crateDetails = Nothing
     }, Cmd.none )
 
 
 type Msg
     = Submit String
     | KeyDown Int
+    | GotCrateDetails (Result Http.Error CrateDetails)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -41,10 +49,34 @@ update msg model =
 
         KeyDown key ->
             if key == 13 then
-                ( { model | crate = model.currentText }, Cmd.none )
+                ( { model | crate = model.currentText }, fetchCrateDetails model.currentText )
             else
                 ( model, Cmd.none )
 
+        GotCrateDetails res ->
+            case res of
+                Ok d ->
+                    ( model, Cmd.none )
+
+                Err e ->
+                    ( model, Cmd.none )
+
+
+fetchCrateDetails : String -> Cmd Msg
+fetchCrateDetails crateName =
+    Http.get
+    { url = "https://crates.io/api/v1/crates/" ++ crateName
+    , expect = Http.expectJson GotCrateDetails decodeCrateDetails
+    }
+
+
+-- Decoders
+
+decodeCrateDetails : D.Decoder CrateDetails
+decodeCrateDetails =
+    D.succeed {}
+
+-- Event handlers
 
 onKeyDown : (Int -> msg) -> Attribute msg
 onKeyDown tagger =
