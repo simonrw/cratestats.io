85350cd53f01fd2e89e28454851c377000ad771f
Simon Walker <s.r.walker101@googlemail.com>
2019-12-06 22:04:55 +0000

    Add handy cache for development

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, jsonify
 import requests
+import logging
+from functools import lru_cache
+
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("cratestats.io")
+logger.setLevel(logging.DEBUG)
 
 
 app = Flask(__name__)
@@ -12,8 +19,15 @@ def index():
 
 @app.route("/api/v1/crates/<crate>")
 def fetch_crates(crate):
+    return jsonify(
+            crates=fetch_crate_data_from_api(crate)
+            )
+
+
+@lru_cache()
+def fetch_crate_data_from_api(crate):
+    logger.debug("fetching crate data for crate %s from crates.io", crate)
     # TODO: injection prevention
     r = requests.get("https://crates.io/api/v1/crates/{}".format(crate))
     r.raise_for_status()
-
-    return jsonify(crates=r.json())
+    return r.json()
