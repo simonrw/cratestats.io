68c2394cfb17da62c08ff2bfba43119e49f3b4e8
Simon Walker <s.r.walker101@googlemail.com>
2019-12-06 15:59:34 +0000

    Create initial search box

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,67 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, h1, input, Attribute, h2)
+import Html.Events exposing (onInput, on, keyCode)
+import Html.Attributes exposing (type_, placeholder)
+import Json.Decode as D
+
+
+type alias Model =
+    { currentText : String
+    , crate : String
+    }
 
 main =
-    Browser.sandbox
-    { init = 0
+    Browser.element
+    { init = init
     , update = update
     , view = view
+    , subscriptions = \_ -> Sub.none
     }
 
 
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { currentText = ""
+    , crate = ""
+    }, Cmd.none )
+
+
 type Msg
-    = Increment
-    | Decrement
+    = Submit String
+    | KeyDown Int
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        Submit text ->
+            ({ model | currentText = text }, Cmd.none )
 
-        Decrement ->
-            model - 1
+        KeyDown key ->
+            if key == 13 then
+                ( { model | crate = model.currentText }, Cmd.none )
+            else
+                ( model, Cmd.none )
 
 
+onKeyDown : (Int -> msg) -> Attribute msg
+onKeyDown tagger =
+    on "keydown" (D.map tagger keyCode)
+
+
+view : Model -> Html Msg
 view model =
     div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    [ h1 [] [ text "Crate Stats" ]
+    , input [ type_ "text", placeholder "Search for crate", onKeyDown KeyDown, onInput Submit ] []
+    , viewCrate model
+    ]
+
+
+viewCrate : Model -> Html Msg
+viewCrate model =
+    div []
+    [ h2 [] [ text model.crate ]
     ]
