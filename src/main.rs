35fd594019b9650ef978978ef142746bdb91d5b8
Simon Walker <s.r.walker101@googlemail.com>
2019-12-15 18:00:57 +0000

    Set up initial test on front page

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
 use actix_web::{App, HttpServer, middleware, web, HttpResponse, Error, error};
 
+fn set_up_tera() -> Result<tera::Tera, tera::Error> {
+    tera::Tera::new("templates/**/*")
+}
+
 async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
     let ctx = tera::Context::new();
     let s = tmpl.render("index.html", &ctx)
@@ -12,16 +16,12 @@ async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     dotenv::dotenv().ok();
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
     HttpServer::new(|| {
-        let tera = match tera::Tera::new("templates/**/*") {
-            Ok(t) => t,
-            Err(e) => {
-                log::error!("could not compile templates: {}", e);
-                std::process::exit(1);
-            }
-        };
+        let tera = set_up_tera().expect("could not set up tera");
 
         App::new()
             .data(tera)
@@ -31,4 +31,31 @@ async fn main() -> io::Result<()> {
     .bind("127.0.0.1:8080")?
     .start()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::dev::Service;
+    use actix_web::{http, test, web, App};
+
+    #[actix_rt::test]
+    async fn test_index() -> Result<(), Error> {
+        // Set up app
+        let tera = set_up_tera().unwrap();
+
+        let mut app = test::init_service(
+            App::new().data(tera).service(web::resource("/").route(web::get().to(index))),
+        ).await;
+
+        // Send request
+        let req = test::TestRequest::get()
+            .uri("/")
+            .to_request();
+        let resp = app.call(req).await?;
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        Ok(())
+    }
 }
