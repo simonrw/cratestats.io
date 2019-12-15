e0fde8f02991a390dd354e8afd68a6430d66c9e6
Simon Walker <s.r.walker101@googlemail.com>
2019-12-15 18:33:41 +0000

    Handle initial JSON api and index handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
 use std::io;
-use actix_web::{App, HttpServer, middleware, web, HttpResponse, Error, error};
 
 fn set_up_tera() -> Result<tera::Tera, tera::Error> {
     tera::Tera::new("templates/**/*")
@@ -9,17 +10,30 @@ fn set_up_tera() -> Result<tera::Tera, tera::Error> {
 
 async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
     let ctx = tera::Context::new();
-    let s = tmpl.render("index.html", &ctx)
+    let s = tmpl
+        .render("index.html", &ctx)
         .map_err(|_| error::ErrorInternalServerError("template error"))?;
 
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
+// API routes
+
+#[derive(Deserialize, Serialize, Debug)]
+struct DownloadTimeseriesRequest {
+    name: String,
+    version: Option<String>,
+}
+
+async fn download_timeseries(item: web::Json<DownloadTimeseriesRequest>) -> HttpResponse {
+    let req = item.0;
+    log::info!("got request: {:?}", req);
+    HttpResponse::Ok().json(req)
+}
+
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     dotenv::dotenv().ok();
-
-    std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
     let port = std::env::var("PORT").expect("PORT variable not set (see .env file)");
@@ -28,9 +42,14 @@ async fn main() -> io::Result<()> {
         let tera = set_up_tera().expect("could not set up tera");
 
         App::new()
-            .data(tera)
             .wrap(middleware::Logger::default())
-            .service(web::resource("/").route(web::get().to(index)))
+            .service(
+                web::scope("/api/v1")
+                    // Limit the size of incoming payload
+                    .data(web::JsonConfig::default().limit(1024))
+                    .route("/downloads", web::post().to(download_timeseries)),
+            )
+            .service(web::scope("/").data(tera).route("", web::get().to(index)))
     })
     .bind(format!("127.0.0.1:{port}", port=port))?
     .start()
@@ -49,13 +68,14 @@ mod tests {
         let tera = set_up_tera().unwrap();
 
         let mut app = test::init_service(
-            App::new().data(tera).service(web::resource("/").route(web::get().to(index))),
-        ).await;
+            App::new()
+                .data(tera)
+                .service(web::resource("/").route(web::get().to(index))),
+        )
+        .await;
 
         // Send request
-        let req = test::TestRequest::get()
-            .uri("/")
-            .to_request();
+        let req = test::TestRequest::get().uri("/").to_request();
         let resp = app.call(req).await?;
 
         assert_eq!(resp.status(), http::StatusCode::OK);
