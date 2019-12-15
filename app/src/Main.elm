e6463874e0ab7322bb7fb0009d6fcb02c7df8bab
Simon Walker <s.r.walker101@googlemail.com>
2019-12-15 22:55:49 +0000

    Implement plotting with elm-vega

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,10 +1,11 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Dict exposing (Dict)
 import Html exposing (Html, button, div, text, h1, input, Attribute, h2, p)
 import Html.Events exposing (onInput, on, keyCode)
 import Html.Attributes exposing (type_, placeholder, id)
+import VegaLite exposing (Spec, dataFromColumns, dataColumn, nums, encoding, position, Position(..), pMType, circle, Measurement(..), pName, toVegaLite)
 
 type alias Model =
   {}
@@ -20,7 +21,24 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {} , Cmd.none )
+    let
+        plotSpec1 =
+            { specs = myVis
+            , id = "#plot-container"
+            }
+
+        plotSpec2 =
+            { specs = myVis
+            , id = "#plot-container2"
+            }
+
+        tasks =
+            Cmd.batch
+            [ elmToJs plotSpec1
+            , elmToJs plotSpec2
+            ]
+    in
+    ( {} , tasks )
 
 
 type Msg
@@ -31,10 +49,38 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update _ model =
   (model, Cmd.none )
 
--- Encoders
-
 view : Model -> Html Msg
 view model =
     div []
     [ h1 [] [ text "CrateStats" ]
+    , div [ id "plot-container" ] []
+    , div [ id "plot-container2" ] []
+    ]
+
+
+type alias PlotSpec =
+    { specs : Spec
+    , id : String
+    }
+
+port elmToJs : PlotSpec -> Cmd msg
+
+-- Visualisations
+
+myVis : Spec
+myVis =
+    let
+        data =
+            dataFromColumns []
+            << dataColumn "x" (nums [10, 20, 30])
+
+        enc =
+            encoding
+                << position X [ pName "x", pMType Quantitative ]
+    in
+    toVegaLite
+    [ VegaLite.title "Hello world"
+    , data []
+    , enc []
+    , circle []
     ]
--- app/src/index.js
+++ app/src/index.js
@@ -3,3 +3,13 @@ import { Elm } from "./Main.elm";
 let app = Elm.Main.init({
   node: document.querySelector("main"),
 });
+
+app.ports.elmToJs.subscribe(data => {
+  let elemId = data.id;
+  let specs = data.specs;
+  console.log(elemId, specs);
+
+  requestAnimationFrame(() => {
+    vegaEmbed(elemId, specs, {actions: false}).catch(console.error);
+  });
+});
