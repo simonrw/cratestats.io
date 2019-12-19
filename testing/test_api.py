89a862557f31931d22e245f3ae2aa0cdba460436
Simon Walker <s.r.walker101@googlemail.com>
2019-12-19 18:46:06 +0000

    Implement test for downloads with no versions

--- testing/test_api.py
+++ testing/test_api.py
@@ -1,18 +1,35 @@
+import warnings
+
+warnings.simplefilter("ignore")
 from starlette.testclient import TestClient
 import pytest
 import sys
+
 sys.path.append(".")
 from server import app
 
 
 def test_downloads():
     with TestClient(app) as client:
-        response = client.post("/api/v1/downloads", json={"name": "fitsio", "version": "0.15.0"})
+        response = client.post(
+            "/api/v1/downloads", json={"name": "fitsio", "version": "0.15.0"}
+        )
         assert response.status_code == 200
-        
+
         json = response.json()
         assert json["name"] == "fitsio"
         assert json["version"] == "0.15.0"
+        assert len(json["downloads"]) > 0
+
+
+def test_downloads_no_version():
+    with TestClient(app) as client:
+        response = client.post("/api/v1/downloads", json={"name": "fitsio"})
+        assert response.status_code == 200
+
+        json = response.json()
+        assert json["name"] == "fitsio"
+        assert json["version"] is None
         assert len(json["downloads"]) > 0
 
 
