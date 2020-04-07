341e1d52515880f906013ccd6690c262eceacdf1
Simon Walker <s.r.walker101@googlemail.com>
2020-04-07 22:52:24 +0100

    Include slider controlling number of crates

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import dotenv
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
 
 dotenv.load_dotenv()
 
@@ -36,6 +38,12 @@ app.layout = html.Div(
         html.Div(
             [
                 html.H2("Crates by category"),
+                dcc.Slider(
+                    id="num-categories-slider",
+                    min=1,
+                    max=len(categories_df),
+                    value=len(categories_df),
+                ),
                 dcc.Graph(
                     id="crate-categories",
                     figure={
@@ -49,10 +57,42 @@ app.layout = html.Div(
                         "layout": {"title": "Crates by category",},
                     },
                 ),
+                dcc.Graph(
+                    id="crate-categories-pie",
+                    figure=go.Figure(
+                        go.Pie(
+                            labels=categories_df["category"],
+                            values=categories_df["crate_count"],
+                        )
+                    ),
+                ),
             ]
         ),
     ]
 )
+
+
+@app.callback(
+    [Output("crate-categories", "figure"), Output("crate-categories-pie", "figure")],
+    [Input("num-categories-slider", "value")],
+)
+def update_num_categories(num_categories):
+    filtered_df = categories_df.head(num_categories)
+    bar_result = {
+        "data": [
+            {
+                "x": filtered_df["category"],
+                "y": filtered_df["crate_count"],
+                "type": "bar",
+            },
+        ],
+        "layout": {"title": "Crates by category",},
+    }
+    pie_result = go.Figure(
+        go.Pie(labels=filtered_df["category"], values=filtered_df["crate_count"],)
+    )
+
+    return bar_result, pie_result
 
 
 if __name__ == "__main__":
