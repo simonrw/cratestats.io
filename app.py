e96db3796ad9953a19aac464e3cb998528b1f5e8
Simon Walker <s.r.walker101@googlemail.com>
2020-04-07 22:44:25 +0100

    Show crates by category

--- app.py
+++ app.py
@@ -1,15 +1,58 @@
+import os
 import dash
+import dotenv
+import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
+
+dotenv.load_dotenv()
+
+
+database_url = os.environ["CRATESTATS_DATABASE_URL"]
+
+# Data setup
+categories_df = pd.read_sql(
+    """
+        SELECT
+            categories.category,
+            count(crates.id) as crate_count
+        FROM crates
+        JOIN crates_categories ON crates.id = crates_categories.crate_id
+        JOIN categories ON crates_categories.category_id = categories.id
+        GROUP BY categories.category
+        ORDER BY crate_count DESC
+        """,
+    database_url,
+)
+
+# App setup
 
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    html.H1("cratestats.io"),
-    dcc.Markdown("""
-    """)
-    ])
+app.layout = html.Div(
+    [
+        html.H1("cratestats.io"),
+        html.Div(
+            [
+                html.H2("Crates by category"),
+                dcc.Graph(
+                    id="crate-categories",
+                    figure={
+                        "data": [
+                            {
+                                "x": categories_df["category"],
+                                "y": categories_df["crate_count"],
+                                "type": "bar",
+                            },
+                        ],
+                        "layout": {"title": "Crates by category",},
+                    },
+                ),
+            ]
+        ),
+    ]
+)
 
 
 if __name__ == "__main__":
